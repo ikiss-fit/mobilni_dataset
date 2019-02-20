9aafde79f8a77ad40229662822ca0a3601d3521c
xkissm00 <xkissm00@stud.fit.vutbr.cz>
2019-02-20 10:49:38 +0100

    update

---
+++ TEST_TRANSCRIPTIONS.sh
@@ -0,0 +1,3 @@
+python3 test_transcriptions.py -d ../../Datasets/mobilni_dataset/LINES_2/chars_2018-11-16.all -t ../../Datasets/mobilni_dataset/TRANSCRIPTION_EVALUATION/transcriptions.txt
+
+eog ../../Datasets/mobilni_dataset/LINES_2/lines.2018-11-16/old-00068_l0061.jpg ../../Datasets/mobilni_dataset/LINES_2/lines.2018-11-16/09ad5f47896e079a13b06384c7e9147fe82e1476_014_614_l0005.jpg ../../Datasets/mobilni_dataset/LINES_2/lines.2018-11-16/05ec75ed9a57b7aaf0cd14006cf423842ae7c518_002_606_l0008.jpg
